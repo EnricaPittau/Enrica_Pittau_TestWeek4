646f309932c8e2dc586b1596a7125532faf1d743
EnricaPittau <101341843+EnricaPittau@users.noreply.github.com>
2022-03-25 15:33:45 +0100

    Add files via upload

---
+++ JavaScriptFilm.js
@@ -0,0 +1,6 @@
+﻿function save() {
+    let storage = getStorage();
+    let nome = document.getElementById("storageName").value;
+    let mail = document.getElementById("storageMail").value;
+    storage.setItem(nome, mail);
+}
